f24e7b75bbf38f20251795a09f6a1df6e2b22c62
abc-sampath <sampath.reddy@abcfinancial.com>
2018-04-03 21:34:20 -0500

    added POC jobs

---
+++ Jobs/POC/POCJob.groovy
@@ -0,0 +1,115 @@
+String git_folder = 'sampathr'
+
+folder("/POC") {
+  displayName('POC')
+  description("Folder for POC Jobs")
+}
+
+def envManage = [
+  [environment: 'dev', services:
+    [
+      [springName: 'address', branch: 'develop'],
+      [springName: 'auth', branch: 'develop'],
+      [springName: 'branding', branch: 'develop'],
+      [springName: 'elastic', branch: 'develop']
+    ]
+  ],
+  [environment: 'qa', services:
+    [
+      [springName: 'address', branch: 'test'],
+      [springName: 'auth', branch: 'test'],
+      [springName: 'branding', branch: 'test'],
+      [springName: 'elastic', branch: 'test']
+    ]
+  ]
+]
+
+envManage.each { config ->
+  String chefEnvironment = config.environment
+
+  for ( apps in config.services ) {
+    String springName = apps.springName
+    String githubBranch = apps.branch
+    String baseFolderName = "/POC/${chefEnvironment}/Spring/"
+    String deployJob = "${baseFolderName}/${springName}/${chefEnvironment}Deploy"
+
+    folder("/POC/${chefEnvironment}") {
+      displayName(chefEnvironment)
+      description("Folder for ${chefEnvironment} env Spring app jobs")
+    }
+
+    folder("/BackEnd/${chefEnvironment}/Spring") {
+      displayName('Spring')
+      description("Folder for all Spring app jobs in ${chefEnvironment}")
+    }
+
+    folder("/BackEnd/${chefEnvironment}/Spring/${springName}") {
+      displayName(springName)
+      description("Folder for all jobs related to ${springName} in ${chefEnvironment}")
+    }
+
+    job("${baseFolderName}${springName}/Restart Services") {
+      properties {
+        buildDiscarder {
+          strategy {
+            logRotator {
+              numToKeepStr('4')
+              daysToKeepStr('')
+              artifactDaysToKeepStr('')
+              artifactNumToKeepStr('')
+            }
+          }
+        }
+      }
+      steps {
+        shell("echo this is restart job for ${springName}")
+      }
+    }
+
+    if ("${chefEnvironment}" == "dev") {
+      job("${baseFolderName}${springName}/Build") {
+        properties {
+          buildDiscarder {
+            strategy {
+              logRotator {
+                numToKeepStr('4')
+                daysToKeepStr('')
+                artifactDaysToKeepStr('')
+                artifactNumToKeepStr('')
+              }
+            }
+          }
+        }
+        steps {
+          shell("echo this is Build job for ${springName}")
+        }
+        publishers {
+          downstream(deployJob, 'SUCCESS')
+        }
+      }
+    }
+
+    job("${deployJob}") {
+      properties {
+        buildDiscarder {
+          strategy {
+            logRotator {
+              numToKeepStr('4')
+              daysToKeepStr('')
+              artifactDaysToKeepStr('')
+              artifactNumToKeepStr('')
+            }
+          }
+        }
+      }
+      steps {
+        shell("echo this is deploy job for ${springName} in ${chefEnvironment}")
+      }
+      if ("${chefEnvironment}" == "dev") {
+        publishers {
+          downstream(deployJob, 'SUCCESS')
+        }
+      }
+    }  
+  }
+}
